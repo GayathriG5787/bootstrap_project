cd4ca97a5e5fcc14abe96aff7ca837c061da8373
GayathriG5787 <gayathrigcse2022@gmail.com>
2026-06-03 13:25:27 +0530

    popover in virtual, hybrid and in person read more button

--- script.js
+++ script.js
@@ -2,9 +2,14 @@ const subscribeBtn = document.getElementById('subscribe')
 const subscribeToast = document.getElementById('subscribeToast')
 
 subscribeBtn.addEventListener("click", () => {
-    
+
     subscribeToast.classList.remove("d-none");
 
     const toast = new bootstrap.Toast(subscribeToast);
     toast.show();
 });
+
+var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+  return new bootstrap.Popover(popoverTriggerEl)
+})
